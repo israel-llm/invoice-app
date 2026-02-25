5d76694167b3b79ab31719f3a5b5d635ab89387e
Israel Llanos <israel.llm@hotmail.com>
2026-02-25 00:08:02 -0500

    Creacion de sub componentes

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
 import { NgClass } from "../../../../node_modules/@angular/common/index";
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { ListItemComponent } from '../list-item/list-item.component';
+import { RowItemComponent } from '../row-item/row-item.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemComponent, RowItemComponent],
   templateUrl: './invoice.component.html'
 })
 export class InvoiceComponent implements OnInit{
